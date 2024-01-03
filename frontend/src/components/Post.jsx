31ff5fc21537f54d4e7077b0832a358e321d295d
lukasilverio94 <lukasilverio94@gmail.com>
2024-01-03 16:48:39 +0100

    styles fixed at post section

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -26,43 +26,49 @@ const Post = ({ post }) => {
   };
 
   return (
-    <div key={post._id} className="flex flex-col gap-y-2 mt-2">
-      <h3 className="text-teal-600 text-3xl ">{post.title}</h3>
-      <p className="text-sm">Where: </p>
-      <p className="text-slate-800 text-2xl "> {post.place}</p>
-      <p>{post.description.slice(0, 25)}...</p>
-      <Link to={`posts/details/${post._id}`} className="my-1">
-        <span className="flex items-center text-teal-700">
-          <span className="hover:border-b hover:border-teal-700">
-            Read more
-          </span>
-          <FaArrowRightLong className="ms-1" />
-        </span>
-      </Link>
-      <small>
-        {formatDistance(new Date(post.createdAt), new Date(), {
-          addSuffix: true,
-        })}{" "}
-        by {post.writer}
-      </small>
+    <div
+      key={post._id}
+      className="w-full flex flex-col border-b-2 pb-5 mt-5 mb-3 md:col-span-1"
+    >
+      <div className="flex flex-col md:flex-row ">
+        <div className="md:w-2/3 flex flex-col gap-y-1 flex-grow">
+          <h3 className="text-teal-600 text-3xl">{post.title}</h3>
+          <div className="my-1">
+            <Stars post={post} onRatingChange={handleRatingChange} />
+          </div>
 
-      {post.image && (
-        <figure>
-          <img
-            className="rounded-md w-full md:w-1/3 mb-4 md:mb-0 md:mr-4"
-            src={post.image}
-            alt="Post"
-          />
-        </figure>
-      )}
+          <p className="text-slate-800 text-2xl"> {post.place}</p>
+          <p>{post.description.slice(0, 25)}...</p>
+          <Link to={`posts/details/${post._id}`} className="my-1">
+            <span className="flex items-center text-teal-700">
+              <span className="hover:border-b hover:border-teal-700">
+                Read more
+              </span>
+              <FaArrowRightLong className="ms-1" />
+            </span>
+          </Link>
+          <small>
+            {formatDistance(new Date(post.createdAt), new Date(), {
+              addSuffix: true,
+            })}{" "}
+            by {post.writer}
+          </small>
+          {/* comment */}
+          <Comments post={post} />
+        </div>
 
-      {/* comment */}
-      <Comments post={post} />
-
-      {/* <Link to={`posts/details/${post._id}`}>Read more...</Link> */}
-      <hr />
-      <Stars post={post} onRatingChange={handleRatingChange} />
-      {/* <Stars post={post} /> */}
+        {post.image && (
+          <div className="md:w-2/3 md:ms-6">
+            <figure>
+              <img
+                className="rounded-md w-full max-w-[500px] mb-4 md:mb-0 md:mr-4"
+                src={post.image}
+                alt="Post"
+              />
+            </figure>
+          </div>
+        )}
+      </div>
     </div>
   );
 };

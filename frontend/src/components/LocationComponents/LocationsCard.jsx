af77862f9b4a145a79ec2bcc4894edeffc799b28
lukasilverio94 <lukasilverio94@gmail.com>
2024-01-09 01:37:36 +0100

    format image validation done 2.0

--- frontend/src/components/LocationComponents/LocationsCard.jsx
+++ frontend/src/components/LocationComponents/LocationsCard.jsx
@@ -13,14 +13,10 @@ export default function LocationCard({
     <div className="bg-white p-6 rounded-lg shadow-md">
       <h4 className="pb-3 text-3xl font-bold text-teal-600">{title}</h4>
       <p className="pb-6 text-gray-600">{description}</p>
-
       <h4 className="pb-3 text-2xl font-bold text-gray-800 flex items-center gap-1">
         <FaMapMarkerAlt className="text-orange-700" />
         <span>{markerIcon}</span>
       </h4>
-
-      <p className="pb-6 text-gray-600">{markerIcon}</p>
-
       <img
         className="w-full h-full max-h-[300px] object-cover bg-center rounded-md shadow-lg"
         src={imageSrc}
